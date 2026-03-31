a7c82c0382a766adf22d4985c4639abf5c17053e
khushi25bce11223-svg <khushi.25bce11223@vitbhopal.ac.in>
2026-03-31 08:39:13 +0530

    Create Srtup_Config.py

---
+++ Srtup_Config.py
@@ -0,0 +1,10 @@
+# Configuration File
+
+# The default webcam for most laptops
+MY_CAMERA_NUMBER = 0
+
+# Where the attendace will be saved
+OUTPUT_FILE = "attendance_list.csv"
+
+#The name of the face detection rule from OpenCV
+DETECTOR_DATA = 'haarcascade_frontalface_default.xml'
